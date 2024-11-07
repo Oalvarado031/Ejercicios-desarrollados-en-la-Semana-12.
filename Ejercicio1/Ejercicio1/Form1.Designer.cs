ff06030505900b123f45aa2a870526348082aaaf
Oalvarado031 <165835512+Oalvarado031@users.noreply.github.com>
2024-11-07 15:37:59 -0600

    Ejercicio 1

---
+++ Ejercicio1/Ejercicio1/Form1.Designer.cs
@@ -0,0 +1,411 @@
+﻿namespace Ejercicio1
+{
+    partial class TITULO
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblCodigo = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblPrecio = new System.Windows.Forms.Label();
+            this.lblCantidad = new System.Windows.Forms.Label();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.lblSubtotal = new System.Windows.Forms.Label();
+            this.lblDescuento = new System.Windows.Forms.Label();
+            this.lblIva = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.txtSubtotal = new System.Windows.Forms.TextBox();
+            this.txtDescuento = new System.Windows.Forms.TextBox();
+            this.txtIva = new System.Windows.Forms.TextBox();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rdb15 = new System.Windows.Forms.RadioButton();
+            this.rdb10 = new System.Windows.Forms.RadioButton();
+            this.rdb5 = new System.Windows.Forms.RadioButton();
+            this.rdb1 = new System.Windows.Forms.RadioButton();
+            this.rdb0 = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Location = new System.Drawing.Point(168, 34);
+            this.lblTitulo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(176, 16);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "COMERCIAL \"EL TRIUNFO\"";
+            // 
+            // lblCodigo
+            // 
+            this.lblCodigo.AutoSize = true;
+            this.lblCodigo.Location = new System.Drawing.Point(56, 89);
+            this.lblCodigo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCodigo.Name = "lblCodigo";
+            this.lblCodigo.Size = new System.Drawing.Size(132, 16);
+            this.lblCodigo.TabIndex = 1;
+            this.lblCodigo.Text = "Código del producto:";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(56, 138);
+            this.lblNombre.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(137, 16);
+            this.lblNombre.TabIndex = 2;
+            this.lblNombre.Text = "Nombre del producto:";
+            this.lblNombre.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lblPrecio
+            // 
+            this.lblPrecio.AutoSize = true;
+            this.lblPrecio.Location = new System.Drawing.Point(56, 192);
+            this.lblPrecio.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPrecio.Name = "lblPrecio";
+            this.lblPrecio.Size = new System.Drawing.Size(49, 16);
+            this.lblPrecio.TabIndex = 3;
+            this.lblPrecio.Text = "Precio:";
+            // 
+            // lblCantidad
+            // 
+            this.lblCantidad.AutoSize = true;
+            this.lblCantidad.Location = new System.Drawing.Point(56, 245);
+            this.lblCantidad.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCantidad.Name = "lblCantidad";
+            this.lblCantidad.Size = new System.Drawing.Size(64, 16);
+            this.lblCantidad.TabIndex = 4;
+            this.lblCantidad.Text = "Cantidad:";
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btnGuardar.Location = new System.Drawing.Point(149, 298);
+            this.btnGuardar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(121, 47);
+            this.btnGuardar.TabIndex = 5;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = false;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // lblSubtotal
+            // 
+            this.lblSubtotal.AutoSize = true;
+            this.lblSubtotal.Location = new System.Drawing.Point(45, 368);
+            this.lblSubtotal.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSubtotal.Name = "lblSubtotal";
+            this.lblSubtotal.Size = new System.Drawing.Size(79, 16);
+            this.lblSubtotal.TabIndex = 6;
+            this.lblSubtotal.Text = "Subtotal   - >";
+            // 
+            // lblDescuento
+            // 
+            this.lblDescuento.AutoSize = true;
+            this.lblDescuento.Location = new System.Drawing.Point(45, 418);
+            this.lblDescuento.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblDescuento.Name = "lblDescuento";
+            this.lblDescuento.Size = new System.Drawing.Size(89, 16);
+            this.lblDescuento.TabIndex = 7;
+            this.lblDescuento.Text = "Descuento  ->";
+            // 
+            // lblIva
+            // 
+            this.lblIva.AutoSize = true;
+            this.lblIva.Location = new System.Drawing.Point(51, 465);
+            this.lblIva.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblIva.Name = "lblIva";
+            this.lblIva.Size = new System.Drawing.Size(48, 16);
+            this.lblIva.TabIndex = 8;
+            this.lblIva.Text = "IVA   ->";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(45, 510);
+            this.lblTotal.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(55, 16);
+            this.lblTotal.TabIndex = 9;
+            this.lblTotal.Text = "Total  ->";
+            // 
+            // txtSubtotal
+            // 
+            this.txtSubtotal.Location = new System.Drawing.Point(149, 368);
+            this.txtSubtotal.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtSubtotal.Name = "txtSubtotal";
+            this.txtSubtotal.Size = new System.Drawing.Size(157, 22);
+            this.txtSubtotal.TabIndex = 10;
+            // 
+            // txtDescuento
+            // 
+            this.txtDescuento.Location = new System.Drawing.Point(149, 418);
+            this.txtDescuento.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtDescuento.Name = "txtDescuento";
+            this.txtDescuento.Size = new System.Drawing.Size(157, 22);
+            this.txtDescuento.TabIndex = 11;
+            // 
+            // txtIva
+            // 
+            this.txtIva.Location = new System.Drawing.Point(149, 465);
+            this.txtIva.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtIva.Name = "txtIva";
+            this.txtIva.Size = new System.Drawing.Size(157, 22);
+            this.txtIva.TabIndex = 12;
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(149, 510);
+            this.txtTotal.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(157, 22);
+            this.txtTotal.TabIndex = 13;
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(220, 89);
+            this.txtCodigo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(157, 22);
+            this.txtCodigo.TabIndex = 14;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(220, 138);
+            this.txtNombre.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(157, 22);
+            this.txtNombre.TabIndex = 15;
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(220, 192);
+            this.txtPrecio.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(157, 22);
+            this.txtPrecio.TabIndex = 16;
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(220, 245);
+            this.txtCantidad.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(157, 22);
+            this.txtCantidad.TabIndex = 17;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btnCalcular.Location = new System.Drawing.Point(355, 356);
+            this.btnCalcular.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(121, 47);
+            this.btnCalcular.TabIndex = 18;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btnLimpiar.Location = new System.Drawing.Point(355, 418);
+            this.btnLimpiar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(121, 47);
+            this.btnLimpiar.TabIndex = 19;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = false;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btnCerrar.Location = new System.Drawing.Point(355, 487);
+            this.btnCerrar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(121, 47);
+            this.btnCerrar.TabIndex = 20;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = false;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdb15);
+            this.groupBox1.Controls.Add(this.rdb10);
+            this.groupBox1.Controls.Add(this.rdb5);
+            this.groupBox1.Controls.Add(this.rdb1);
+            this.groupBox1.Controls.Add(this.rdb0);
+            this.groupBox1.Location = new System.Drawing.Point(457, 58);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(151, 249);
+            this.groupBox1.TabIndex = 21;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Descuento";
+            // 
+            // rdb15
+            // 
+            this.rdb15.AutoSize = true;
+            this.rdb15.Location = new System.Drawing.Point(8, 202);
+            this.rdb15.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rdb15.Name = "rdb15";
+            this.rdb15.Size = new System.Drawing.Size(57, 20);
+            this.rdb15.TabIndex = 22;
+            this.rdb15.TabStop = true;
+            this.rdb15.Text = "15 %";
+            this.rdb15.UseVisualStyleBackColor = true;
+            // 
+            // rdb10
+            // 
+            this.rdb10.AutoSize = true;
+            this.rdb10.Location = new System.Drawing.Point(8, 159);
+            this.rdb10.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rdb10.Name = "rdb10";
+            this.rdb10.Size = new System.Drawing.Size(57, 20);
+            this.rdb10.TabIndex = 3;
+            this.rdb10.TabStop = true;
+            this.rdb10.Text = "10 %";
+            this.rdb10.UseVisualStyleBackColor = true;
+            // 
+            // rdb5
+            // 
+            this.rdb5.AutoSize = true;
+            this.rdb5.Location = new System.Drawing.Point(8, 118);
+            this.rdb5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rdb5.Name = "rdb5";
+            this.rdb5.Size = new System.Drawing.Size(50, 20);
+            this.rdb5.TabIndex = 2;
+            this.rdb5.TabStop = true;
+            this.rdb5.Text = "5 %";
+            this.rdb5.UseVisualStyleBackColor = true;
+            // 
+            // rdb1
+            // 
+            this.rdb1.AutoSize = true;
+            this.rdb1.Location = new System.Drawing.Point(8, 78);
+            this.rdb1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rdb1.Name = "rdb1";
+            this.rdb1.Size = new System.Drawing.Size(50, 20);
+            this.rdb1.TabIndex = 1;
+            this.rdb1.TabStop = true;
+            this.rdb1.Text = "1 %";
+            this.rdb1.UseVisualStyleBackColor = true;
+            // 
+            // rdb0
+            // 
+            this.rdb0.AutoSize = true;
+            this.rdb0.Location = new System.Drawing.Point(8, 34);
+            this.rdb0.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rdb0.Name = "rdb0";
+            this.rdb0.Size = new System.Drawing.Size(50, 20);
+            this.rdb0.TabIndex = 0;
+            this.rdb0.TabStop = true;
+            this.rdb0.Text = "0 %";
+            this.rdb0.UseVisualStyleBackColor = true;
+            // 
+            // TITULO
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGoldenrodYellow;
+            this.ClientSize = new System.Drawing.Size(661, 565);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.txtIva);
+            this.Controls.Add(this.txtDescuento);
+            this.Controls.Add(this.txtSubtotal);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblIva);
+            this.Controls.Add(this.lblDescuento);
+            this.Controls.Add(this.lblSubtotal);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.lblCantidad);
+            this.Controls.Add(this.lblPrecio);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.lblCodigo);
+            this.Controls.Add(this.lblTitulo);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "TITULO";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MINI FACTURA";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblCodigo;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblPrecio;
+        private System.Windows.Forms.Label lblCantidad;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Label lblSubtotal;
+        private System.Windows.Forms.Label lblDescuento;
+        private System.Windows.Forms.Label lblIva;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.TextBox txtSubtotal;
+        private System.Windows.Forms.TextBox txtDescuento;
+        private System.Windows.Forms.TextBox txtIva;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnCerrar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rdb15;
+        private System.Windows.Forms.RadioButton rdb10;
+        private System.Windows.Forms.RadioButton rdb5;
+        private System.Windows.Forms.RadioButton rdb1;
+        private System.Windows.Forms.RadioButton rdb0;
+    }
+}
+
---
+++ Ejercicio1/Ejercicio1/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio1
+{
+    public partial class TITULO : Form
+    {
+        public TITULO()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Datos leidos correctamente");
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double descuento = 0;
+            double cantidad = int.Parse(txtCantidad.Text);
+            double precio = int.Parse(txtPrecio.Text);
+            double subtotal = cantidad * precio;
+            txtSubtotal.Text = subtotal.ToString(); 
+
+            if (rdb0.Checked )
+            {
+                descuento = 0;
+            } 
+            else if (rdb1.Checked )
+            {
+                descuento = 0.01;
+            }
+            else if (rdb5.Checked)
+            {
+                descuento = 0.05;
+            } 
+            else if (rdb10.Checked)
+            {
+                descuento = 0.1;
+            }
+            else if (rdb15.Checked)
+            {
+                descuento = 0.15;
+            }
+            double descuentoAplicado = subtotal * descuento;
+            txtDescuento.Text = descuentoAplicado.ToString();
+
+            double iva = subtotal * 0.15;
+            txtIva.Text = iva.ToString();
+
+            double total = (subtotal + iva) - descuentoAplicado;
+            txtTotal.Text = total.ToString();
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtCantidad.Clear();
+            txtPrecio.Clear();
+            txtSubtotal.Clear();
+            txtDescuento.Clear();
+            txtIva.Clear();
+            txtTotal.Clear();
+            txtNombre.Clear();
+            txtCodigo.Clear();  
+
+            rdb0.Checked = false;
+            rdb1.Checked = false;
+            rdb5.Checked = false;
+            rdb10.Checked = false;
+            rdb15.Checked = false;
+
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Nombre: Oscar Ivan Alvarado Jiron, 07/11/2024");
+            Close();    
+        }
+    }
+}
